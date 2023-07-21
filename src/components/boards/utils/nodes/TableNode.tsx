f699c0590cda0b77ad8b2ac0a0aef14b4dffee7b
lu-css <dossantossilvalucas963@gmail.com>
2023-07-21 16:38:48 -0300

    feat: change table color

--- src/components/boards/utils/nodes/TableNode.tsx
+++ src/components/boards/utils/nodes/TableNode.tsx
@@ -9,11 +9,45 @@ import { PropertyType } from '../editNodes/TablePropertie'
 import TableNodeModel from '@/models/TableNodeModel'
 import TablePropertyModel from '@/models/TablePropertyModel'
 
+enum TABLE_COLOR {
+  violet = 'bg-violet-500',
+  red = 'bg-red-500',
+  blue = 'bg-blue-500',
+  yellow = 'bg-yellow-500',
+  green = 'bg-green-500',
+}
+
+enum TABLE_COLOR_HOVER {
+  violet = 'bg-violet-700',
+  red = 'bg-red-700',
+  blue = 'bg-blue-700',
+  yellow = 'bg-yellow-700',
+  green = 'bg-green-700',
+}
+
 const TableNode = ({ selected, id, dragging, data, xPos, yPos }: NodeProps) => {
   const [hovered, setHovered] = useState(false)
 
   const [inputName, setInputName] = useState(data.table ? data.table.name ?? '' : '')
   const [properties, setProperties] = useState<PropertyType[]>(data.table ? data.table.properties ?? [] : [])
+  const [tableColor, setTableColor] = useState(2)
+
+  const colorLoop = [TABLE_COLOR.violet, TABLE_COLOR.red, TABLE_COLOR.blue, TABLE_COLOR.yellow, TABLE_COLOR.green]
+  const colorHoverLoop = [TABLE_COLOR_HOVER.violet, TABLE_COLOR_HOVER.red, TABLE_COLOR_HOVER.blue, TABLE_COLOR_HOVER.yellow, TABLE_COLOR_HOVER.green]
+
+  function nextColor() {
+    if (tableColor + 1 > colorLoop.length) {
+      setTableColor(0)
+    }
+
+    setTableColor(oldColor => {
+      if (oldColor + 1 >= colorLoop.length) {
+        return 0
+      }
+
+      return oldColor + 1
+    })
+  }
 
   function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
     const t = event.target.value
@@ -23,12 +57,6 @@ const TableNode = ({ selected, id, dragging, data, xPos, yPos }: NodeProps) => {
   function tablePropertiesChange(changes: PropertyType[]) {
     setProperties(changes)
   }
-
-  useEffect(() => {
-    const storedData = localStorage.getItem('data');
-    if (storedData) {
-    }
-  }, []);
 
   // Save data to localStorage whenever the data state changes
   useEffect(() => {
@@ -40,8 +68,10 @@ const TableNode = ({ selected, id, dragging, data, xPos, yPos }: NodeProps) => {
 
   const squareNameClass = `text-center bg-transparent border border-b-0 border-t-0 ${inputName === '' ? ' border-white' : 'border-transparent'}  outline-none text-3x1 text-bold`
 
+  const propertyCircleColor = colorLoop[tableColor] == TABLE_COLOR.yellow ? TABLE_COLOR.green : TABLE_COLOR.yellow
+
   return (
-    <div id={id} className={`bg-violet-500 rounded-2xl w-full h-full min-w-[200px] min-h-[100px] node flex items-center justify-center text-white font-bold p-4 relative hover:bg-violet-700 ${selected ? "bg-violet-700" : ""}`}
+    <div id={id} className={`${colorLoop[tableColor]} rounded-2xl w-full h-full min-w-[200px] min-h-[100px] node flex items-center justify-center text-white font-bold p-4 relative ${'hover:' + colorHoverLoop[tableColor]} ${selected ? colorHoverLoop[tableColor] : ""}`}
       onMouseEnter={() => { setHovered(true) }}
       onMouseLeave={() => { setHovered(false) }}>
       <TableControll id={id} onRemove={data.onRemoveNode} visible={hovered && !dragging} />
@@ -51,9 +81,16 @@ const TableNode = ({ selected, id, dragging, data, xPos, yPos }: NodeProps) => {
         isVisible={selected}
         minWidth={200}
         minHeight={100} />
+
+      <div className='bottom-2 h-3 w-[80%] absolute flex items-center'>
+        <span
+          className='cursor-pointer'
+          onClick={nextColor}
+        >color</span>
+      </div>
       <div className='top-2 h-3 w-[80%]  absolute flex items-center'>
         {properties.map(property => (
-          <span className={`h-2 w-2 ${property.columnName?.endsWith('_id') ? "bg-yellow-500" : 'bg-white'} rounded-full mr-2`}></span>
+          <span className={`h-2 w-2 ${property.columnName?.endsWith('_id') ? propertyCircleColor : 'bg-white'} rounded-full mr-2`}></span>
         ))}
       </div>
 
